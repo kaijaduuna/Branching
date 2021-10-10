4b9804f0c284461a95fdc59c6a6bb282d8c30498
kaijaduuna <ikaunis@gmail.com>
2021-10-10 22:19:15 +0300

    Switch

--- src/Time.java
+++ src/Time.java
@@ -1,10 +1,11 @@
 public class Time {
 
     public static void main(String[] args) {
+        String regex = "([01]?[0-9]|2[0-4])";
         int time = 20;
-        if (time <= 12)
+        if (time <= 12) {
             System.out.println("Good Morning, Sunshine!");
-        else if (time >= 13 && time <= 19)
+        } else if (time >= 13 && time <= 19)
             System.out.println("Good Afternoon! Work hard!");
         else if (time >= 20 && time <= 24)
             System.out.println("Good Evening! Get some rest!");
